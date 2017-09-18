1d5b02e189e9082da12828ee5e3b4de09f41ee09
JaehyeonSK <cjh7163@naver.com>
2017-09-18 23:16:00 +0900

    solve

--- 02_Encryption/02_Encryption/Program.cs
+++ 02_Encryption/02_Encryption/Program.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace _02_Encryption
 {
+    class Cryptography
+    {
+        public long encrypt(int[] numbers)
+        {
+            long answer = 1;
+
+            Array.Sort(numbers);
+            numbers[0] += 1;
+
+            foreach (int n in numbers)
+            {
+                answer *= n;
+            }
+
+            return answer;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+
         }
     }
 }
